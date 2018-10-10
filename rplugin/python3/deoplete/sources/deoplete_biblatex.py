9a6bf9ae62105cb30b21c52ca906e305bbb7d0f6
Lion <extra.lion.w@gmail.com>
2018-10-10 12:45:42 +0200

    Added option to reload bibliography after a filechange.

--- rplugin/python3/deoplete/sources/deoplete_biblatex.py
+++ rplugin/python3/deoplete/sources/deoplete_biblatex.py
@@ -29,28 +29,46 @@ class Source(Base):
         self.mark = '[bib]'
         self.name = 'biblatex'
 
-    def on_init(self, context):
-        self.__bib_file = context['vars'].get(
-            'deoplete#sources#biblatex#bibfile',
-            '~/bibliography.bib'
-        )
-        self.__bib_file = os.path.abspath(os.path.expanduser(self.__bib_file))
+    @property
+    def __bibliography(self):
+        if self.__reload_bibfile_on_change:
+            mtime = os.stat(self.__bib_file).st_mtime
+            if mtime != self.__bib_file_mtime:
+                self.__bib_file_mtime = mtime
+                self.__read_bib_file()
+
+        return self.__bibliography_cached
+
+    def __read_bib_file(self):
         try:
             with open(self.__bib_file) as bf:
                 parser = bibtexparser.bparser.BibTexParser(
                     ignore_nonstandard_types=False,
                 )
                 parser.customization = bibtexparser_customizations
-                self.__bibliography = bibtexparser.load(
+                bibliography = bibtexparser.load(
                     bf,
                     parser=parser,
                 ).entries_dict
+
+                self.__bibliography_cached = bibliography
         except FileNotFoundError:
             self.vim.err_write(
                 '[deoplete-biblatex] No such file: {0}\n'.format(
                     self.__bib_file,
                 ),
             )
+
+    def on_init(self, context):
+        bib_file = context['vars'].get(
+            'deoplete#sources#biblatex#bibfile',
+            '~/bibliography.bib'
+        )
+        bib_file = os.path.abspath(os.path.expanduser(bib_file))
+
+        self.__bib_file = bib_file
+        self.__bib_file_mtime = os.stat(bib_file).st_mtime
+        self.__read_bib_file()
 
         pattern_delimiter = context['vars'].get(
             'deoplete#sources#biblatex#delimiter',
@@ -68,6 +86,14 @@ class Source(Base):
             + pattern_completed
             + pattern_current
         )
+
+        reload_bibfile_on_change = context['vars'].get(
+            'deoplete#sources#biblatex#reloadbibfileonchange',
+            0,
+        )
+        reload_bibfile_on_change = bool(reload_bibfile_on_change)
+
+        self.__reload_bibfile_on_change = reload_bibfile_on_change
 
     def gather_candidates(self, context):
         if self.__pattern.search(context['input']):
