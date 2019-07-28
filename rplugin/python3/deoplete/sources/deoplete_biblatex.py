bffec71874678d52f54fc37770aa4a33117c4c98
Lion <extra.lion.w@gmail.com>
2019-07-28 09:23:41 +0200

    Add common BibTexParser options

--- rplugin/python3/deoplete/sources/deoplete_biblatex.py
+++ rplugin/python3/deoplete/sources/deoplete_biblatex.py
@@ -1,39 +1,77 @@
 #!/usr/bin/env
-import re
+# Standard Library
 import os
+import re
 
-from .base import Base
-from deoplete.util import load_external_module
-
+# Third Party
 import bibtexparser
 
-load_external_module(__file__, 'sources/deoplete_biblatex')
+# This Module
+from deoplete.logger import getLogger
+from deoplete.util import load_external_module
 
-MY_CODES = {
-    'lion:': 'test',
-    'tandem:': 'test',
-}
+# Local Folder
+from .base import Base
+
+load_external_module(__file__, "sources/deoplete_biblatex")
+
+logger = getLogger(__name__)
+
+
+# MY_CODES = {"lion:": "test", "tandem:": "test"}
+
+
+class CustomizationError(Exception):
+    pass
 
 
 def bibtexparser_customizations(record):
     record = bibtexparser.customization.author(record)
     record = bibtexparser.customization.editor(record)
-    record = bibtexparser.customization.add_plaintext_fields(record)
+    try:
+        record = bibtexparser.customization.add_plaintext_fields(record)
+    except AttributeError:
+        logger.debug(
+            (
+                "Could add plaintext fields for record with ID: {}. "
+                "This will be ignored."
+            ).format(record["ID"])
+        )
+        pass
+
     return record
+
+
+BIBTEXPARSER_BOOL_OPTIONS = {
+    "common_strings",
+    "add_missing_from_crossref",
+    "interpolate_strings",
+    "homogenize_fields",
+    "ignore_nonstandard_types",
+}
+
+BIBTEXPARSER_DEFAULTS_KWARGS = {
+    "ignore_nonstandard_types": False,
+    "common_strings": True,
+}
 
 
 class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
 
-        self.filetypes = ['rst', 'markdown']
-        self.mark = '[bib]'
-        self.name = 'biblatex'
+        self.filetypes = ["rst", "markdown"]
+        self.mark = "[bib]"
+        self.name = "biblatex"
+
+        self.parser_kwargs = BIBTEXPARSER_DEFAULTS_KWARGS.copy()
+        self.options = {}
 
     @property
     def __bibliography(self):
-        if self.__reload_bibfile_on_change:
+        if self.options["reload_bibfile_on_change"]:
             mtime = os.stat(self.__bib_file).st_mtime
+
             if mtime != self.__bib_file_mtime:
                 self.__bib_file_mtime = mtime
                 self.__read_bib_file()
@@ -43,105 +81,106 @@ class Source(Base):
     def __read_bib_file(self):
         try:
             with open(self.__bib_file) as bf:
-                parser = bibtexparser.bparser.BibTexParser(
-                    ignore_nonstandard_types=False,
-                )
+                parser = bibtexparser.bparser.BibTexParser(**self.parser_kwargs)
                 parser.customization = bibtexparser_customizations
-                bibliography = bibtexparser.load(
-                    bf,
-                    parser=parser,
-                ).entries_dict
+                bibliography = bibtexparser.load(bf, parser=parser).entries_dict
 
                 self.__bibliography_cached = bibliography
         except FileNotFoundError:
             self.vim.err_write(
-                '[deoplete-biblatex] No such file: {0}\n'.format(
-                    self.__bib_file,
-                ),
+                "[deoplete-biblatex] No such file: {0}\n".format(self.__bib_file)
             )
 
     def on_init(self, context):
-        bib_file = context['vars'].get(
-            'deoplete#sources#biblatex#bibfile',
-            '~/bibliography.bib'
+        bib_file = context["vars"].get(
+            "deoplete#sources#biblatex#bibfile", "~/bibliography.bib"
         )
         bib_file = os.path.abspath(os.path.expanduser(bib_file))
+
+        custom_parser_kwargs = {
+            opt: context["vars"].get("deoplete#sources#biblatex#{}".format(opt), None)
+            for opt in BIBTEXPARSER_BOOL_OPTIONS
+        }
+        custom_parser_kwargs = {
+            k: bool(v) for k, v in custom_parser_kwargs.items() if v is not None
+        }
+        self.parser_kwargs.update(custom_parser_kwargs)
 
         self.__bib_file = bib_file
         self.__bib_file_mtime = os.stat(bib_file).st_mtime
         self.__read_bib_file()
 
-        pattern_delimiter = context['vars'].get(
-            'deoplete#sources#biblatex#delimiter',
-            ',',
+        pattern_delimiter = context["vars"].get(
+            "deoplete#sources#biblatex#delimiter", ","
         )
-        pattern_start = context['vars'].get(
-            'deoplete#sources#biblatex#startpattern',
-            r'\[(?:[\w,]+:)?',
+        pattern_start = context["vars"].get(
+            "deoplete#sources#biblatex#startpattern", r"\[(?:[\w,]+:)?"
         )
-        pattern_key = context['vars'].get(
-            'deoplete#sources#biblatex#keypattern',
-            r'@?[\w-]+',
+        pattern_key = context["vars"].get(
+            "deoplete#sources#biblatex#keypattern", r"@?[\w-]+"
         )
 
-        pattern_current = r'{}$'.format(pattern_key)
-        pattern_completed = r'(?:{}{})*'.format(pattern_key, pattern_delimiter)
+        pattern_current = r"{}$".format(pattern_key)
+        pattern_completed = r"(?:{}{})*".format(pattern_key, pattern_delimiter)
 
-        self.__pattern = re.compile(
-            pattern_start
-            + pattern_completed
-            + pattern_current
-        )
+        self.__pattern = re.compile(pattern_start + pattern_completed + pattern_current)
 
-        reload_bibfile_on_change = context['vars'].get(
-            'deoplete#sources#biblatex#reloadbibfileonchange',
-            0,
-        )
-        reload_bibfile_on_change = bool(reload_bibfile_on_change)
+        self.__get_bool_options(context, ["reload_bibfile_on_change", "add_info"])
 
-        self.__reload_bibfile_on_change = reload_bibfile_on_change
+    def __get_bool_options(self, context, options):
+        """
+        Get boolean options from the configuration and store them in the format "__<option>"
+        In the configuration they should be given without the underscores.
+        """
 
-        add_info = context['vars'].get(
-            'deoplete#sources#biblatex#addinfo',
-            0,
-        )
-        add_info = bool(add_info)
-
-        self.__add_info = add_info
+        for opt in options:
+            value = context["vars"].get(
+                "deoplete#sources#biblatex#{}".format(opt.replace("_", "")), 0
+            )
+            value = bool(value)
+            self.options[opt] = value
 
     def __format_info(self, entry):
-        return '{title}{author}{date}'.format(
-            title=('Title: {}\n'.format(entry['plain_title'])
-                   if 'plain_title' in entry else ''),
-            author=(
-                'Author{plural}: {author}\n'.format(
-                    plural='s' if len(entry['author']) > 1 else '',
-                    author='; '.join(entry['author']),
-                )
-                if 'author' in entry else ''
+        return "{title}{author}{date}".format(
+            title=(
+                "Title: {}\n".format(entry["plain_title"])
+                if "plain_title" in entry
+                else ""
             ),
-            date=('Year: {}\n'.format(entry['plain_date'].split('-')[0])
-                  if 'plain_date' in entry else ''),
+            author=(
+                "Author{plural}: {author}\n".format(
+                    plural="s" if len(entry["author"]) > 1 else "",
+                    author="; ".join(entry["author"]),
+                )
+                if "author" in entry
+                else ""
+            ),
+            date=(
+                "Year: {}\n".format(entry["plain_date"].split("-")[0])
+                if "plain_date" in entry
+                else ""
+            ),
         )
 
     def __entry_to_candidate(self, entry):
         candidate = {
-            'abbr': entry['ID'],
-            'word': entry['ID'],
-            'kind': entry['ENTRYTYPE'],
+            "abbr": entry["ID"],
+            "word": entry["ID"],
+            "kind": entry["ENTRYTYPE"],
         }
 
-        if self.__add_info:
-            candidate['info'] = self.__format_info(entry)
+        if self.options["add_info"]:
+            candidate["info"] = self.__format_info(entry)
 
         return candidate
 
     def gather_candidates(self, context):
-        if self.__pattern.search(context['input']):
+        if self.__pattern.search(context["input"]):
             candidates = [
                 self.__entry_to_candidate(entry)
                 for entry in self.__bibliography.values()
             ]
+
             return candidates
         else:
             return []
